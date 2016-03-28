078b7aaaab6b67ca5ac312a45b8c70774d62dc3b
zhangyuecl <2698051674@qq.com>
2016-03-28 09:56:22 +0800

    userm

--- Manager/UserInfoManager.cs
+++ Manager/UserInfoManager.cs
@@ -63,7 +63,32 @@ namespace Manager
             return OutputHelper.GetOutputResponse(ResultCode.OK);
         }
 
-        
+        /// <summary>
+        /// 修改个人信息
+        /// </summary>
+        /// <param name="userInfo"></param>
+        /// <returns></returns>
+        public bool Update(UserInfoDt userInfo)
+        {
+            return userServer.Update(userInfo);
+        }
+        //public bool Delete(UserInfoDt userInfo)
+        //{
+        //    return userServer.Delete(userInfo.UserId);
+        //}
+
+        /// <summary>
+        /// 根据邮箱获取用户对象
+        /// </summary>
+        /// <param name="loginId"></param>
+        /// <returns></returns>
+        public OutputModel Get(string loginId)
+        {
+            UserInfoDt u = userServer.GetUserInfo(loginId);
+            if (u == null)
+                return OutputHelper.GetOutputResponse(ResultCode.NoData);
+            return OutputHelper.GetOutputResponse(ResultCode.OK, u);
+        }
     }
 }
 
