3478cacdf3364d3e3c8abbe56e28bde90fff6a9b
zhangyuecl <2698051674@qq.com>
2016-03-28 11:58:56 +0800

    点赞server

--- Server/ScanOrSupportServer.cs
+++ Server/ScanOrSupportServer.cs
@@ -3,10 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Tool;
+using DataTrsfer;
+using EF;
 namespace Server
 {
-    class ScanOrSupportServer
+    public class ScanOrSupportServer : BaseService<ScanOrSupport>
     {
+        public bool Add(ScanOrSupportDt scanorsupport)
+        {
+            base.Add(TransferObject.ConvertObjectByEntity<ScanOrSupportDt, ScanOrSupport>(scanorsupport));
+            return Save() > 0;
+        }
+        public bool Update(ScanOrSupportDt scanorsupport)
+        {
+            base.Update(TransferObject.ConvertObjectByEntity<ScanOrSupportDt, ScanOrSupport>(scanorsupport));
+            return Save() > 0;
+        }
+        public bool Delete(int id)
+        {
+            base.Delete(id);
+            return Save() > 0;
+        }
+        public bool Delete(ScanOrSupportDt scanorsupport)
+        {
+            base.Delete(TransferObject.ConvertObjectByEntity<ScanOrSupportDt, ScanOrSupport>(scanorsupport));
+            return Save() > 0;
+        }
+        public ScanOrSupportDt Get(int id)
+        {
+            return TransferObject.ConvertObjectByEntity<ScanOrSupport, ScanOrSupportDt>(base.Select(id));
+        }
+        /// <summary>
+        /// 获取某照片某用户点赞记录
+        /// </summary>
+        /// <param name="photoid"></param>
+        /// <param name="userid"></param>
+        /// <returns></returns>
+        public ScanOrSupportDt Get(int photoid, string userid)
+        {
+            return TransferObject.ConvertObjectByEntity<ScanOrSupport, ScanOrSupportDt>(base.Select(o => o.PhotoId == photoid && o.UserId == userid).FirstOrDefault());
+        }
+        /// <summary>
+        /// 获取某用户的点赞记录
+        /// </summary>
+        /// <param name="userid"></param>
+        /// <returns></returns>
+        public List<ScanOrSupportDt> GetList(string userid)
+        {
+            return TransferObject.ConvertObjectByEntity<ScanOrSupport, ScanOrSupportDt>(base.Select(o => o.UserId == userid).ToList());
+        }
+        /// <summary>
+        /// 获取某照片的点赞记录
+        /// </summary>
+        /// <param name="photoid"></param>
+        /// <returns></returns>
+        public List<ScanOrSupportDt> GetList(int photoid)
+        {
+            return TransferObject.ConvertObjectByEntity<ScanOrSupport, ScanOrSupportDt>(base.Select(o => o.PhotoId == photoid).ToList());
+        }
+        public List<ScanOrSupportDt> GetList()
+        {
+            return TransferObject.ConvertObjectByEntity<ScanOrSupport, ScanOrSupportDt>(base.Select(o => true).ToList());
+        }
     }
 }
