1023e8cf7ab8f371c817e002e00de5d457fa6e90
zhangyuecl <2698051674@qq.com>
2016-03-30 17:17:32 +0800

    cs

--- Server/LocationServer.cs
+++ Server/LocationServer.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EF;
+using DataTrsfer;
+using Tool;
 
 namespace Server
 {
-    class LocationServer
+    public class LocationServer:BaseService<Location>
     {
+        public bool Add(LocationDt l)
+        {
+             base.Add(TransferObject.ConvertObjectByEntity<LocationDt, Location>(l));
+             return Save() > 0;
+        }
+        public bool Update(LocationDt l)
+        {
+            base.Update(TransferObject.ConvertObjectByEntity<LocationDt, Location>(l));
+            return Save() > 0;
+        }
+        public bool Delete(int id)
+        {
+            base.Delete(id);
+            return Save() > 0;
+        }
     }
 }
