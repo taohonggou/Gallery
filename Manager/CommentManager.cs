fbf152010828b9a230ac6fee1442a0469b0420ca
zhangyuecl <2698051674@qq.com>
2016-04-12 16:32:01 +0800

    comment

--- Manager/CommentManager.cs
+++ Manager/CommentManager.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Server;
+using Tool;
+using DataTrsfer;
 
 namespace Manager
 {
-    class CommentManager
+    public class CommentManager
     {
+        private CommentServer server = ObjectContainer.GetInstance<CommentServer>();
+        public OutputModel Add(CommentDt comment)
+        {
+            if (comment == null)
+                return OutputHelper.GetOutputResponse(ResultCode.NoParameter);
+            if (server.Add(comment))
+                return OutputHelper.GetOutputResponse(ResultCode.OK);
+            return OutputHelper.GetOutputResponse(ResultCode.Error);
+        }
+        public OutputModel Delete(int id)
+        {
+            bool b = false;
+            List<CommentDt> list = server.GetSon(id);
+            if (list.Count <= 0)
+                b = true;
+            else
+            {
+                foreach (CommentDt c in list)
+                {
+                    if (Delete(c.CommentId).StatusCode != 1)
+                        return OutputHelper.GetOutputResponse(ResultCode.Error);
+                }
+            }
+            if (server.Delete(id) && b)
+                return OutputHelper.GetOutputResponse(ResultCode.OK);
+            return OutputHelper.GetOutputResponse(ResultCode.Error);
+        }
+        public OutputModel GetByPhotoId(int photoId)
+        {
+            List<CommentDt> list =server.GetByPhotoId(photoId);
+            if (list.Count<=0)
+                return OutputHelper.GetOutputResponse(ResultCode.NoData);
+            return OutputHelper.GetOutputResponse(ResultCode.OK, list);
+        }
     }
 }
--- Server/CommentServer.cs
+++ Server/CommentServer.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataTrsfer;
+using EF;
+using Tool;
 
 namespace Server
 {
-    class CommentServer
+    public class CommentServer : BaseService<Comment>
     {
+        public bool Add(CommentDt comment)
+        {
+            base.Add(TransferObject.ConvertObjectByEntity<CommentDt, Comment>(comment));
+            return base.Save() > 0;
+        }
+        public bool Delete(int id)
+        {
+            base.Delete(id);
+            return base.Save() > 0;
+        }
+        public bool Delete(List<CommentDt> list)
+        {
+            base.Delete(TransferObject.ConvertObjectByEntity<CommentDt, Comment>(list));
+            return base.Save() > 0;
+        }
+        public bool Update(CommentDt comment)
+        {
+            base.Update(TransferObject.ConvertObjectByEntity<CommentDt, Comment>(comment));
+            return base.Save() > 0;
+        }
+        /// <summary>
+        /// 获取子评论
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public List<CommentDt> GetSon(int id)
+        {
+            return TransferObject.ConvertObjectByEntity<Comment, CommentDt>(base.Select(o => o.UpId == id).ToList());
+        }
+        public List<CommentDt> GetByPhotoId(int photoId)
+        {
+            return TransferObject.ConvertObjectByEntity<Comment, CommentDt>(base.Select(o => o.PhotoId == photoId).ToList());
+        }
     }
 }
