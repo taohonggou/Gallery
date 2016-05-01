ea711d21a883bc8435219ee5c31acd5b99bd283c
taohonggou <cl889521@163.com>
2016-05-01 11:22:19 +0800

    完成了获取最新图片的页面

---
+++ GalleryUI/Scripts/photo.js
@@ -0,0 +1,94 @@
+﻿var pageIndex = 1, pageSize = 10;
+var flag = true;
+$(document).ready(function () {
+    loadPhotosFirst(pageIndex, pageSize);
+
+    //var $container = $('#con1_1');
+    //$container.imagesLoaded(function () {
+    //    $container.masonry({
+    //        itemSelector: '.product_list',
+    //        columnWidth: 5 //每两列之间的间隙为5像素
+    //    });
+    //});
+
+    $(window).scroll(function () {
+        var scrollTop = document.body.scrollTop;
+        var clientHeight = document.documentElement.clientHeight;
+        var scrollHeight = document.body.scrollHeight;
+        if ( 100>= scrollHeight - clientHeight-scrollTop && flag) {
+            pageIndex++;
+            loadPhotos(pageIndex, pageSize);
+        }
+    });
+
+    //滚动条滚动到离底部距离50的时候触发
+
+
+
+});
+
+//测试获取列表
+function loadPhotos(index, size, isFirst) {
+    var data = '';
+    $.ajax({
+        url: '/Photo/GetRecentPhotos',
+        type: 'get',
+        data: { pageIndex: index, pageSize: size },
+        async: false,
+        success: function (data) {
+            if (data.StatusCode == 1) {
+                data = initPhotos(data.Data.List);
+                var $boxes = $(data);
+                $container.append($boxes).masonry('appended', $boxes, true);
+                $container.imagesLoaded(function () {
+                    $container.masonry();
+                });//加载完图片后，会实现自动重新排列。【这里是重点】
+
+
+            }
+            else {
+                flag = false;
+                return layer.msg(data.Message);
+            }
+
+        }
+
+    });
+    return data;
+};
+var $container;
+function loadPhotosFirst(index, size) {
+    $.ajax({
+        url: '/Photo/GetRecentPhotos',
+        type: 'get',
+        data: { pageIndex: index, pageSize: size },
+        success: function (data) {
+            if (data.StatusCode == 1) {
+                var str = initPhotos(data.Data.List);
+                $('#con1_1').html(str);
+
+                $container = $('#con1_1');
+                $container.imagesLoaded(function () {
+                    $container.masonry({
+                        itemSelector: '.product_list',
+                        columnWidth: 5 //每两列之间的间隙为5像素
+                    });
+                });
+                $container.imagesLoaded(function () {
+                    $container.masonry();
+                });//加载完图片后，会实现自动重新排列。【这里是重点】
+            }
+            else
+                return layer.msg(data.Message);
+        }
+
+    });
+};
+
+function initPhotos(json) {
+    var str = '';
+    $.each(json, function () {
+        str += '<div class="product_list"><a href="#"><img src="' + this.ImgUrl + '"  alt="" /></a><p>' + this.Name + '</p></div>';
+    })
+    return str;
+}
